c3d63e58f0924a384ac04023633611c1030fbc4b
Sahebjeet singh <87801221+SahebjeetSINGH@users.noreply.github.com>
2022-12-13 22:26:42 +0530

    Add files via upload

---
+++ basic of hashing/HighestLowestFreq.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int main()
+{
+    int n = 1e7;
+    vector<int> hash(n,0);
+
+    int s;
+    cin>>s;
+    int max=0 , maxi, min=1e7,mini;
+    int a[s];
+    for(int i=0;i<s;i++)
+    {
+        cin>>a[i];
+        hash[a[i]]++;
+        if (max<hash[a[i]]){
+            max=hash[a[i]];
+            maxi=i;
+        }
+        else{
+            min=hash[a[i]];
+            mini=i;
+        }
+
+    }
+    
+    cout<<" Maximum "<<hash[maxi]<<" "<<max<<endl;
+    cout<<" Minimum "<<hash[mini]<<" "<<min<<endl;
+
+
+}
